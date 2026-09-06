88d2c3528f534bfee95df4b7ca41afb4f249e5e1
M8kiTHappen <mohamed.a03@outlook.com>
2026-09-06 18:25:42 -0500

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> pairs = new HashMap<>();
+
+        pairs.put(']', '[');
+        pairs.put('}', '{');
+        pairs.put(')', '(');
+
+        for(char c : s.toCharArray()){
+            if(pairs.containsKey(c)){
+
+                if(stack.isEmpty() || stack.pop() != pairs.get(c)){
+                    return false;
+                
+                }
+                
+            }
+            else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
